79628169816cac1636bdc795b335fffd36ede056
Nisha <nishakumari@dbs.com>
2022-12-05 15:59:28 +0530

    5/12/2022

---
+++ QuizApp/app/src/main/java/com/dbs/quizapp/activity/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.dbs.quizapp.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import com.dbs.quizapp.R
+import com.dbs.quizapp.model.Constants
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonStart:Button = findViewById(R.id.btn_start)
+        val etName = findViewById<TextView>(R.id.et_name).toString()
+        buttonStart.setOnClickListener {
+            if (etName.isEmpty()){
+                Toast.makeText(this,"Please Enter Your Name",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this@MainActivity, QuizQuestionActivity::class.java)
+                // TODO Pass the name through intent using the constant variable which we have created.
+                intent.putExtra(Constants.USER_NAME, etName)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
+
+
---
+++ QuizApp/app/src/main/java/com/dbs/quizapp/activity/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.dbs.quizapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dbs.quizapp.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
