b3264e4458ebaf1e2989827d685a8148ace1efff
Nisha <nishakumari@dbs.com>
2022-12-05 16:20:41 +0530

    5/12/2022

--- QuizApp/app/src/main/java/com/dbs/quizapp/activity/MainActivity.kt
+++ QuizApp/app/src/main/java/com/dbs/quizapp/activity/MainActivity.kt
@@ -18,14 +18,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonStart:Button = findViewById(R.id.btn_start)
-        val etName = findViewById<TextView>(R.id.et_name).toString()
+        val etName:AppCompatEditText = findViewById(R.id.et_name)
         buttonStart.setOnClickListener {
-            if (etName.isEmpty()){
+            if (etName.text.toString().isEmpty()){
                 Toast.makeText(this,"Please Enter Your Name",Toast.LENGTH_SHORT).show()
             }else{
                 val intent = Intent(this@MainActivity, QuizQuestionActivity::class.java)
                 // TODO Pass the name through intent using the constant variable which we have created.
-                intent.putExtra(Constants.USER_NAME, etName)
+                intent.putExtra(Constants.USER_NAME, etName.text.toString())
                 startActivity(intent)
                 finish()
             }
--- QuizApp/app/src/main/java/com/dbs/quizapp/activity/ResultActivity.kt
+++ QuizApp/app/src/main/java/com/dbs/quizapp/activity/ResultActivity.kt
@@ -2,11 +2,20 @@ package com.dbs.quizapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.dbs.quizapp.R
+import com.dbs.quizapp.model.Constants
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val username = findViewById<TextView>(R.id.tv_name)
+        username?.text = intent.extras?.getString(Constants.USER_NAME)
+        val total = intent.getIntExtra(Constants.TOTAL_QUESTIONS,10)
+        val correct = intent.getIntExtra(Constants.CORRECT_ANSWERS,0)
+        val score = findViewById<TextView>(R.id.tv_score)
+        score?.text= "Your Score is $correct out of $total"
+
     }
 }
